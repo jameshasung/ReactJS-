ec78852d2858cc40694fdf64881bad6f56f5f867
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-07 19:57:56 +0900

    6.2 completed

--- react-for-practice/src/App.js
+++ react-for-practice/src/App.js
@@ -1,19 +1,35 @@
 import React, { useState, useEffect } from "react";
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => {
     setCounter(counter + 1);
+  };
+  const onChange = (e) => {
+    setKeyword(e.target.value);
   };
   const runOnce = () => {
     console.log("runOnce");
   };
   useEffect(runOnce, []);
   useEffect(() => {
-    console.log("useEffect");
-  }, []);
-
+    console.log("counter changed");
+  }, [counter]);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5)
+      console.log("search for ", keyword);
+  }, [keyword]);
+  useEffect(() => {
+    console.log("all changed");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
