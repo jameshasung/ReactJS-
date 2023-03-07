bc33321b92d8bf4fb8fa804ff12bcbc7268415e4
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-07 20:09:03 +0900

    chapter6 completed

--- react-for-practice/src/App.js
+++ react-for-practice/src/App.js
@@ -1,39 +1,20 @@
 import React, { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("I'm created :)");
+    return () => console.log("I'm destroyed :(");
+  }, []);
+  return <h1>hello</h1>;
+}
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => {
-    setCounter(counter + 1);
-  };
-  const onChange = (e) => {
-    setKeyword(e.target.value);
-  };
-  const runOnce = () => {
-    console.log("runOnce");
-  };
-  useEffect(runOnce, []);
-  useEffect(() => {
-    console.log("counter changed");
-  }, [counter]);
-  useEffect(() => {
-    if (keyword !== "" && keyword.length > 5)
-      console.log("search for ", keyword);
-  }, [keyword]);
-  useEffect(() => {
-    console.log("all changed");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(!showing);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing && <Hello />}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"} </button>
     </div>
   );
 }
-
 export default App;
