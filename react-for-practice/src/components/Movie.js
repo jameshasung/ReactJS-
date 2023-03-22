1e14737d637cb4b6daa4cd1f70c21c7ab2ce2aad
jameshasung <117165485+jameshasung@users.noreply.github.com>
2023-03-22 19:29:11 +0900

    chapter7 complete

--- react-for-practice/src/components/Movie.js
+++ react-for-practice/src/components/Movie.js
@@ -1,31 +1,33 @@
-import protoTypes from "prop-types";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styles from "./Movie.module.css";
 
-function Movie({ id, coverImg, title, summary, genres }) {
+function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
-      </h2>
-      <img src={coverImg} alt={title} />
-      <p>{summary}</p>
-      <ul>
-        <li>
-          {genres.map((genre) => (
-            <li key={genre}>{genre}</li>
+    <div className={styles.movie}>
+      <img src={coverImg} alt={title} className={styles.movie__img} />
+      <div>
+        <h2 className={styles.movie__title}>
+          <Link to={`/movie/${id}`}>{title}</Link>
+        </h2>
+        <h3 className={styles.movie__year}>{year}</h3>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
+        <ul className={styles.movie__genres}>
+          {genres.map((g) => (
+            <li key={g}>{g}</li>
           ))}
-        </li>
-      </ul>
+        </ul>
+      </div>
     </div>
   );
 }
 
-Movie.protoTypes = {
-  id: protoTypes.number.isRequired,
-  coverImg: protoTypes.string.isRequired,
-  title: protoTypes.string.isRequired,
-  summary: protoTypes.string.isRequired,
-  genres: protoTypes.arrayOf(protoTypes.string).isRequired,
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Movie;
--- react-for-practice/src/routes/Detail.js
+++ react-for-practice/src/routes/Detail.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
-
 function Detail() {
   const { id } = useParams();
   const getMovie = async () => {
@@ -9,11 +8,9 @@ function Detail() {
     ).json();
     console.log(json);
   };
-
   useEffect(() => {
     getMovie();
   }, []);
   return <h1>Detail</h1>;
 }
-
 export default Detail;
